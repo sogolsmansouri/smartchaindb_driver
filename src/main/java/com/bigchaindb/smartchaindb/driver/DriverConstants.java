a218b547c32c63f97f00f25d6fd7b568143513d7
smansou2 <smansou2@ncsu.edu>
2024-10-13 19:40:44 -0400

    first commit

---
+++ src/main/java/com/bigchaindb/smartchaindb/driver/DriverConstants.java
@@ -0,0 +1,28 @@
+package com.bigchaindb.smartchaindb.driver;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DriverConstants {
+    // Generic constants
+    protected static final String SMARTCHAINDB_PUBKEY = "7EAsHUGQ15LdS2NoX9543bgEoZg7BmkVqdxSmiSda3Dv";
+    protected static final List<String> VALIDATOR_NODES = Arrays.asList("http://159.203.77.155:9984/",
+            "http://209.97.144.32:9984/", "http://159.203.166.36:9984/", "http://209.97.154.89:9984/");
+
+    // Kafka-related constants
+    protected static final String KAFKA_BROKERS = "152.7.99.165:9092";
+    protected static final String OFFSET_RESET_EARLIER = "earliest";
+    protected static final Integer MAX_POLL_RECORDS = 1;
+    protected static final String COMMON_TOPIC = "SystemQueue";
+
+    // Stardog-related constants
+    protected static final String SERVER = "http://152.7.177.221:5820";
+    protected static final String ADMIN_USERNAME = "admin";
+    protected static final String ADMIN_PASSWORD = "admin";
+    protected static final String PROCESS_DB = "MARCO-ProcessTaxonomy";
+    protected static final String PRODUCT_DB = "MARCO-Product";
+    protected static final String COMMON_DB = "MARCO-Common";
+
+    protected static final String GROUP_MANAGER_ENDPOINT = "http://localhost:8080/GJoin";
+    protected static final boolean GROUP_SIGNATURE = false;
+}
